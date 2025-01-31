6a56ab14f2a4026a6173bf4b988cc576eb81f285
gbeyer4094 <gbeyer4094@gmail.com>
2025-01-30 23:12:07 -0600

    Created Element Matching

---
+++ src/main/java/org/example/ElementMatching.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class ElementMatching {
+    public static void main(String[] args) {
+        int[] array1 = {1, 2, 3, 4, 5};
+        int[] array2 = {3, 4, 5, 6, 7};
+
+        for (int i = 0; i < array1.length; i++) {
+            for (int j = 0; j < array2.length; j++) {
+                if (array1[i] == array2[j]) {
+                    System.out.println("Match found: " + array1[i]);
+                }
+            }
+        }
+    }
+
+}
